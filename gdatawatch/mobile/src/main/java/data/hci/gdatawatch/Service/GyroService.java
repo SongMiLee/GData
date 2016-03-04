f6557d0a225d24cf7918d00879a5133f224b71c7
SongMiLee <s91010@naver.com>
2016-03-04 14:37:26 +0900

    convert to smart watch

---
+++ gdatawatch/mobile/src/main/java/data/hci/gdatawatch/Service/GyroService.java
@@ -0,0 +1,66 @@
+package data.hci.gdatawatch.Service;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.IBinder;
+
+import data.hci.gdatawatch.Global.StaticVariable;
+
+
+public class GyroService extends Service implements SensorEventListener {
+
+    SensorManager mSensorManager;
+    Sensor gyroSensor;
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    public int onStartCommand(Intent intent, int flags, int startId){
+        mSensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);   //센서 매니저 사용
+        gyroSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+
+        mSensorManager.registerListener(this,gyroSensor,SensorManager.SENSOR_DELAY_NORMAL);
+
+        return START_NOT_STICKY;
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    public void onDestroy(){
+        super.onDestroy();
+        mSensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if(event.sensor.getType() == gyroSensor.TYPE_GYROSCOPE){
+            float x = event.values[0];
+            float y = event.values[1];
+            float z = event.values[2];
+
+            //다른 액티비티들에게 내용을 보낸다.
+            Intent broadcastIntent = new Intent();
+            broadcastIntent.setAction(StaticVariable.BROADCAST_GYRO);
+
+            broadcastIntent.putExtra("x", x);
+            broadcastIntent.putExtra("y", y);
+            broadcastIntent.putExtra("z", z);
+
+            sendBroadcast(broadcastIntent);
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+}
