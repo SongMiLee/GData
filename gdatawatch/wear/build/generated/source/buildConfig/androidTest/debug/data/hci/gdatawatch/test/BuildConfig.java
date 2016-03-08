4b4784131fd590dffcbe071c80aca931eff35b28
siy93 <dlsduq5996@naver.com>
2016-03-08 23:56:26 +0900

    smart watch update

---
+++ gdatawatch/wear/build/generated/source/buildConfig/androidTest/debug/data/hci/gdatawatch/test/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package data.hci.gdatawatch.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "data.hci.gdatawatch.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
